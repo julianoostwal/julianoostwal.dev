b6acee5903fcbd477a28ba1987edaf982c6956c9
Julian Oostwal <info@julianoostwal.dev>
2025-04-19 11:09:21 +0000

    Revert "sentry"
    This reverts commit c96d80eb3149c5034c2d3b9b2d49bca7389d2b7c.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -13,28 +12,4 @@ const nextConfig = {
       },
 };
 
-export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-org: "flitsert",
-project: "julianoostwaldev",
-
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
-
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-});
+export default nextConfig;
